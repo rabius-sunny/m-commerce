0361bfc6614b868d09ca1eb5a7b0ba97eefc9829
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-01 03:44:20 +0600

    update: title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Next Mart</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
